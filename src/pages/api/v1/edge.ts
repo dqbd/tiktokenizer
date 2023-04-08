544204c593b643d556c8d8b26ca4093c9baac75a
Tat Dat Duong <david@duong.cz>
2023-04-08 20:11:39 +0200

    Add edge example endpoint, upgrade to 1.0.4

---
+++ src/pages/api/v1/edge.ts
@@ -0,0 +1,23 @@
+// @ts-expect-error
+import wasm from "@dqbd/tiktoken/lite/tiktoken_bg.wasm?module";
+import model from "@dqbd/tiktoken/encoders/cl100k_base.json";
+import { init, Tiktoken } from "@dqbd/tiktoken/lite/init";
+
+export const config = { runtime: "edge" };
+
+async function handler(req: Request) {
+  await init((imports) => WebAssembly.instantiate(wasm, imports));
+
+  const encoding = new Tiktoken(
+    model.bpe_ranks,
+    model.special_tokens,
+    model.pat_str
+  );
+
+  const tokens = encoding.encode("hello world");
+  encoding.free();
+
+  return new Response(`${tokens}`);
+}
+
+export default handler;
