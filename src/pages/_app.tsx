740c40322f0b7cd57c2839f1bd6d81a81d72d886
Tat Dat Duong <david@duong.cz>
2023-04-11 18:24:03 +0200

    Add Vercel Analytics for audience monitoring

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
-
+import { Analytics } from "@vercel/analytics/react";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
