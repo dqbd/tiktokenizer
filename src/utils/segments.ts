bf76e40c4851f0f5fe2dd9d53f73d052e17e7c4a
Tat Dat Duong <david@duong.cz>
2024-03-14 16:46:27 +0100

    Fix reverse grapheme matching for HF tokenizers

--- src/utils/segments.ts
+++ src/utils/segments.ts
@@ -7,10 +7,7 @@ const graphemer = new Graphemer();
 
 export type Segment = {
   text: string;
-  tokens: {
-    id: number;
-    idx: number;
-  }[];
+  tokens: { id: number; idx: number }[];
 };
 
 export function getTiktokenSegments(
@@ -52,50 +49,30 @@ export function getHuggingfaceSegments(
   const tokens = tokenizer.encode(inputText);
   // remove first token, which is always <s>?
   tokens.shift();
-  console.log("HF tokens", tokens);
-  const te = new TextEncoder();
+
   const segments: { text: string; tokens: { id: number; idx: number }[] }[] =
     [];
 
-  let byteAcc: number[] = [];
-  let tokenAcc: { id: number; idx: number }[] = [];
   let inputGraphemes = graphemer.splitGraphemes(inputText);
-
   const tokenStrs = tokenizer.model.convert_ids_to_tokens(tokens);
-  const dec = tokenizer.decoder ?? ((x: string) => x);
-  console.log("decoder", dec, dec.config);
-  return tokenStrs.map((token, idx) => ({
-    // This is extremely dumb and I hate it. Oh, sentencepiece!
-    text: idx == 0 ? dec([token]) : dec([" ", token]),
-    tokens: [{ id: tokens[idx]!, idx }],
-  }));
+
+  let startTokenIdx = 0;
+  let tokenAcc: { id: number; idx: number }[] = [];
 
   for (let idx = 0; idx < tokens.length; idx++) {
-    const token = tokens[idx]!;
-    const single = tokenizer.decode_single([token], {
-      clean_up_tokenization_spaces: false,
-      skip_special_tokens: false,
-    });
-    console.log("HF single", single);
-    byteAcc.push(...te.encode(single));
-    tokenAcc.push({ id: token, idx });
+    tokenAcc.push({ id: tokens[idx]!, idx });
 
-    // const segmentText = textDecoder.decode(new Uint8Array(byteAcc));
-    // const graphemes = graphemer.splitGraphemes(segmentText);
-    const graphemes = graphemer.splitGraphemes(single);
-    console.log("HF graphemes", graphemes, "input", inputGraphemes);
+    const prev = tokenizer.decoder(tokenStrs.slice(0, startTokenIdx + 1));
+    const curr = tokenizer.decoder(tokenStrs.slice(0, idx + 1));
+
+    const segmentText = prev === curr ? curr : curr.slice(prev.length);
+    const graphemes = graphemer.splitGraphemes(segmentText);
 
     if (graphemes.every((item, idx) => inputGraphemes[idx] === item)) {
-      segments.push({ text: single, tokens: tokenAcc });
+      segments.push({ text: segmentText, tokens: tokenAcc });
 
-      byteAcc = [];
       tokenAcc = [];
-      inputGraphemes = inputGraphemes.slice(graphemes.length);
-    } else {
-      console.log("HF mismatch", graphemes, inputGraphemes);
-      segments.push({ text: single, tokens: tokenAcc });
-      byteAcc = [];
-      tokenAcc = [];
+      startTokenIdx = idx;
       inputGraphemes = inputGraphemes.slice(graphemes.length);
     }
   }
